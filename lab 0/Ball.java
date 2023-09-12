3cb1851eee7e36af6512d6afaa402572a92f3a6d
Felicia <felicianovac4@gmail.com>
2023-09-12 18:56:53 +0300

    create one more class and structure the main class

---
+++ lab 0/Ball.java
@@ -0,0 +1,22 @@
+public class Ball {
+    public boolean isBouncing;
+    private int speed;
+    private String color;
+
+    public Ball(boolean isBouncing, int speed, String color) {
+        this.isBouncing = isBouncing;
+        this.speed = speed;
+        this.color = color;
+    }
+
+    public void informState() {
+        String state = isBouncing ? "bouncing" : "standing";
+        String speedInfo = isBouncing ? " with a speed of " + speed + "m/s" : "";
+        System.out.println("The ball is " + state + speedInfo + " and has color " + color);
+    }
+
+    public void separateActions(){
+        System.out.println("----------------------------------------");
+    }
+
+}
--- lab 0/Main.java
+++ lab 0/Main.java
@@ -1,9 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        GameObject obj1 = new GameObject(true, 10, "red");
-        GameObject obj2 = new GameObject(false, 0, "blue");
+        Ball obj1 = new Ball(true, 2, "red");
+        Ball obj2 = new Ball(false, 0, "blue");
+
+        Player player1 = new Player("Ana");
+        Player player2 = new Player("Maxim");
+
 
         obj1.informState();
         obj2.informState();
+        obj2.separateActions();
+
+        player1.countBounces(10);
+        player2.countBounces(0);
+
+        player1.displayScore();
+        player2.displayScore();
     }
 }
