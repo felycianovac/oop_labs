50df42462766c64644846001d6f48a8fa98d5df1
Felicia <felicianovac4@gmail.com>
2023-11-07 13:18:42 +0200

    update storage [no significant changes]

--- src/lab_2/directory/Person.py
+++ src/lab_2/directory/Person.py
@@ -6,3 +6,6 @@ class Person:
     def say_hello(self):
         print(f"Hello, my name is {self.name} and I am {self.age} years old.")
 
+    def say_something():
+    printf("Hello, I am a person")
+
--- src/lab_2/tracker/DetectorScheduler.java
+++ src/lab_2/tracker/DetectorScheduler.java
@@ -1,8 +1,5 @@
 package lab_2.tracker;
 
-import lab_2.files.Document;
-
-import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
